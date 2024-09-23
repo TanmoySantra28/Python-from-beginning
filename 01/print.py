a9fb141a47be4f624a8164af8cef9b91e84a451c
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-24 01:04:24 +0530

    print statement, comments and escape sequences

--- 01/print.py
+++ 01/print.py
@@ -1,6 +1,16 @@
-#print statement
+#Comment a single line
+"""
+    Multiline
+    Comment
+"""
 #print is used to print or display anything.
 print("Hello World")
 print(12)
 print("Seven means",7)
-print("\n")
+#escape sequence "\n"
+#double quote character \"...\"
+print("\"Good Morning\" and\nGood Night")
+#separator : separates objects within print statement
+#end : specify what to print at the end
+print("Tanmoy","Santra",28, sep="$", end="!!")
+print("Good Night")
