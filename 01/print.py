3ade671a208df4524d65b0b1bc1ac906dc9c5a32
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-23 22:18:33 +0530

    print statement

---
+++ 01/print.py
@@ -0,0 +1,4 @@
+#print statement
+print("Hello World")
+print(12)
+print("Seven means",7)
--- 04 Lecture 4/main.py
+++
@@ -1,5 +0,0 @@
-#print is a function, used to print anything
-print("Hello World")
-print(28)
-print("Number Seven",7)
-print(20+30)
