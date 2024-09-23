c2bf98554f9fd147ede381df075047a2dbe2b361
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-24 03:00:55 +0530

    variables and datatypes

---
+++ 02/main.py
@@ -0,0 +1,15 @@
+a = 10 #int
+b = 2.6781910 #float
+c = complex(2,3) #complex
+d = True #boolean
+e = "Tanmoy" #string
+f = None #NoneType
+list1 = [2, 1.5, [9,8], "Tanmoy", "Santra"] #list
+tuple1 = (5, 8.3, ("Lion", "Tiger")) #tuple
+dict1 = {"name":"Cat", "colour":"white", "age":2} #dictionary
+print("Type of a = ",type(a),"Value of a = ",a)
+print("Type of b = ",type(b),"Value of b = ",b)
+print("Type of c = ",type(c),"Value of c = ",c)
+print("Type of d = ",type(d),"Value of d = ",d)
+print("Type of e = ",type(e),"Value of e = ",e)
+print("Type of f = ",type(f),"Value of f = ",f)
