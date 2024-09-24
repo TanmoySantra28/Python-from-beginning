21de6079c4c71e486e92e2f677ef24ad423ffbfe
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-24 05:34:36 +0530

    taking user input

---
+++ 05/calc.py
@@ -0,0 +1,15 @@
+#Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!
+a = int(input("1st number = "))
+b = int(input("2nd number = "))
+res1 = a+b
+res2 = a-b
+res3 = a*b
+res4 = a/b
+res5 = a//b
+res6 = a%b
+print("Addition = ",res1)
+print("Subtraction = ",res2)
+print("Product = ",res3)
+print("Division = ",res4)
+print("Floor division = ",res5)
+print("Remainder = ",res6)
---
+++ 05/main.py
@@ -0,0 +1,11 @@
+name = input("Enter your first name = ")
+s_name = input("Enter your last name = ")
+print("Name is = ",name,s_name,sep=" ") #concatenation
+
+a = int(input("1st number = ")) #input as int
+b = int(input("2nd number = "))
+print("Answer = ",a+b)
+
+x = input("1st number = ") #input as string
+y = input("2nd number = ")
+print("Answer = ",int(a)*int(b)) #typecasting
