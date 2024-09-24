89accfe55861acbf2ec6d53aad691dadbbe77974
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-24 09:08:48 +0530

    strings in python

---
+++ 06/main.py
@@ -0,0 +1,15 @@
+str = "Tanmoy"
+str1 = 'Santra'
+print(str)
+print(str1)
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+print(str[5])
+for c in str: #loop``
+    print(c, end=" ")
+a = '''\nPython is an "interpreted" language,
+       and easily to rub'''
+print(a);
