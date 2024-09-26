2c55cb53d5ee7bd869523629cc2b752ecfdcfedf
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-26 16:14:09 +0530

    string methods

--- 08/main.py
+++ 08/main.py
@@ -54,3 +54,21 @@ str1 = "ABCDTANMO09!^"
 print("Isprintable = ",str1.isprintable())
 str2 = "ANjkdoe\n"
 print("Isprintable = ",str2.isprintable())
+#isspace
+str1 = "    "
+print("IsSpace = ",str1.isspace())
+str2 = "ANjkdoe "
+print("IsSpace = ",str2.isspace())
+#istitle
+str1 = "This Is White Oud"
+print("Ititle = ",str1.istitle())
+str2 = "ANjkdoe "
+print("Istitle = ",str2.istitle())
+#startswith
+print("Startswith \"Tan\" = ",str.startswith("Tan"));
+print("Startswith \"San\" = ",str.startswith("San"));
+#swapcase
+print("Swapcase = ",str.swapcase())
+#title
+str1 = "it is a white cat"
+print(str1.title())
