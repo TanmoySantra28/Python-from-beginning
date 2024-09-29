9073998f37db1afdd60441fd5b928a7440bb5ab0
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-29 23:57:53 +0530

    while loop

---
+++ 12/main.py
@@ -0,0 +1,7 @@
+n = int(input("Enter number = "))
+i = 1
+while (i<=n): # when value of i, you will enter is lesser than n's value, it will exit from the while loop
+    i = int(input("Enter number = "))
+    print(i)
+print("Exited from the loop")
+
---
+++ 12/main2.py
@@ -0,0 +1,10 @@
+i = 1
+while(i<=10):
+    print(i, end=" ")
+    i=i+1
+
+print()
+j = 10
+while(j>=1):
+    print(j, end=" ")
+    j=j-1
