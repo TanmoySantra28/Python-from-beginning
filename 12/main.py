5b714ce3759de9341b453bbea9206fadf0f30aa0
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-09-30 00:11:38 +0530

    while loop

--- 12/main.py
+++ 12/main.py
@@ -3,5 +3,3 @@ i = 1
 while (i<=n): # when value of i, you will enter is lesser than n's value, it will exit from the while loop
     i = int(input("Enter number = "))
     print(i)
-print("Exited from the loop")
-
---
+++ 12/main3.py
@@ -0,0 +1,6 @@
+i = 10
+while (i>0):
+    print(i, end=" ")
+    i=i-1
+else:
+    print("\nEnd of while, you are in else now")
